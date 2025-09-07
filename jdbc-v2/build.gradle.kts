de8fc93fcd38c71329816226e7e38b5c3d74fd6e
Marc ROZANC <marc@rozanc.fr>
2025-09-07 23:23:53 +0200

    Remove comments

--- jdbc-v2/build.gradle.kts
+++ jdbc-v2/build.gradle.kts
@@ -47,24 +47,3 @@ tasks.jar {
 tasks.sourcesJar {
     dependsOn(tasks.generateGrammarSource)
 }
-
-//tasks.shadowJar {
-//    archiveClassifier = "all"
-//    dependencies {
-//        exclude(dependency("io.micrometer:.*"))
-//        exclude(dependency("org.slf4j:.*"))
-//    }
-//    val shadeBasePackage = "${project.group}.shaded"
-//    relocate("net.jpountz", "${shadeBasePackage}.net.jpountz.lz4")
-//    relocate("org.ow2", "${shadeBasePackage}.org.ow2")
-//    relocate("org.roaringbitmap", "${shadeBasePackage}.org.roaringbitmap")
-//    relocate("org.objectweb", "${shadeBasePackage}.org.objectweb")
-//    manifest {
-//        attributes["Automatic-Module-Name"] = "com.clickhouse.client.http"
-//    }
-//    exclude("**/module-info.class")
-//}
-//
-//tasks.assemble {
-//    dependsOn(tasks.shadowJar)
-//}
